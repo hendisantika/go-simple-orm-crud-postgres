2b3ce457dd1fa7744c96186d91fbea85be54a776
hendisantika <hendisantika@yahoo.co.id>
2024-08-24 10:47:42 +0700

    feat: Add CreatePayment 🫸🌀✏️📗🐧🐳

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"go-simple-orm-crud-postgres/models"
+	"gorm.io/gorm"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
+func CreatePayment(db *gorm.DB, payment models.Payment) (int64, error) {
+	result := db.Create(&payment)
+	if result.RowsAffected == 0 {
+		return 0, errors.New("payment not created")
+	}
+	return result.RowsAffected, nil
+}
 func main() {
 	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
 	// to see how GoLand suggests fixing it.
