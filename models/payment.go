24f3a58d9fb3ea0f8a9a711fa051b62aef08321d
hendisantika <hendisantika@yahoo.co.id>
2024-08-24 10:46:34 +0700

    Add Payment models 🫸🌀✏️📗🐧🐳

---
+++ models/payment.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Payment struct {
+	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey;"`
+	PaymentCode string
+	Name        string
+	Status      string
+	Created     time.Time `gorm:"autoCreateTime"`
+	Updated     time.Time `gorm:"autoUpdateTime"`
+}
