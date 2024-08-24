f250d00cd787f1cfd224721700f660c19c2ea250
hendisantika <hendisantika@yahoo.co.id>
2024-08-24 10:39:13 +0700

    Add Setup 🫸🌀✏️📗🐧🐳

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"gorm.io/gorm"
+)
+
 const (
 	Host     = "localhost"
 	User     = "yuji"
@@ -7,3 +12,19 @@ const (
 	Name     = "payment"
 	Port     = "5432"
 )
+
+func Setup() (*gorm.DB, error) {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		Host,
+		Port,
+		User,
+		Name,
+		Password,
+	)
+
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
